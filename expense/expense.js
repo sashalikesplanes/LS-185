7582603049574fe139667f5c7ae42b2defa969b8
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-26 20:54:54 +0200

    changed to be a class based app

--- expense/expense.js
+++ expense/expense.js
@@ -3,86 +3,97 @@
 const { Client } = require("pg");
 const { argv, exit } = require("node:process");
 
-config = {
+db_config = {
   database: "expense",
   host: "/tmp",
   user: "sasha",
   port: 5432,
 };
 
-function logAndExit(error) {
-  console.log(`Error expense.js | ${error}`);
-  exit(1);
+class CLI {
+  constructor() {
+    this.expenseData = new ExpenseData(db_config);
+  }
+  main(argv) {
+    if (argv[2] === "list") {
+      this.expenseData.printAllExpenses();
+    } else if (argv[2] === "add") {
+      this.expenseData.addExpense(argv[3], argv[4]);
+    } else if (argv[2] === "clear") {
+      console.log("deleting all expenses");
+    } else {
+      this.printHelp();
+    }
+  }
+  printHelp() {
+    console.log(`
+  An expense recording system
+  
+  Commands:
+  
+  add AMOUNT MEMO [DATE] - record a new expense
+  clear - delete all expenses
+  list - list all expenses
+  delete NUMBER - remove expense with id NUMBER
+  search QUERY - list expenses with a matching memo field`);
+  }
 }
 
-async function getAllExpenses() {
-  const client = new Client(config);
-  try {
-    await client.connect();
+class ExpenseData {
+  constructor(config) {
+    this.config = config;
+    this.client = new Client(config);
+  }
+
+  logAndExit(error) {
+    console.log(`Error expense.js | ${error}`);
+    exit(1);
+  }
+
+  async getAllExpenses() {
+    await this.client.connect().catch(this.logAndExit);
 
     const expenses = (
-      await client.query("SELECT * FROM expenses ORDER BY created_on ASC;")
+      await this.client
+        .query("SELECT * FROM expenses ORDER BY created_on ASC;")
+        .catch(this.logAndExit)
     ).rows;
 
-    client.end();
+    this.client.end().catch(this.logAndExit);
 
     return await expenses;
-  } catch (e) {
-    client.end();
-    logAndExit(e);
+  }
+
+  async printAllExpenses() {
+    const expenses = await this.getAllExpenses();
+
+    expenses.forEach((expense) => {
+      const textColumns = [
+        String(expense.id),
+        expense.created_on.toDateString(),
+        String(expense.amount).padStart(15, " "),
+        String(expense.memo),
+      ];
+
+      console.log(textColumns.join(" | "));
+    });
+  }
+
+  async addExpense(amount, memo) {
+    // TODO validate inputs
+    if (amount === undefined || memo === undefined) {
+      console.log("You must provide an amount and a memo");
+      return;
+    }
+
+    const queryText = `INSERT INTO expenses(amount, memo, created_on) VALUES ($1, $2, NOW());`;
+    const queryArgs = [amount, memo];
+
+    await this.client.connect().catch(this.logAndExit);
+    await this.client.query(queryText, queryArgs).catch(this.logAndExit);
+    this.client.end().catch(this.logAndExit);
   }
 }
 
-async function printAllExpenses() {
-  const expenses = await getAllExpenses();
-
-  expenses.forEach((expense) => {
-    const textColumns = [
-      String(expense.id),
-      expense.created_on.toDateString(),
-      String(expense.amount).padStart(15, " "),
-      String(expense.memo),
-    ];
-
-    console.log(textColumns.join(" | "));
-  });
-}
-
-async function addExpense(amount, memo) {
-  // TODO validate inputs
-  if (amount === undefined || memo === undefined) {
-    console.log("You must provide an amount and a memo");
-    return;
-  }
-
-  query = `INSERT INTO expenses(amount, memo, created_on) VALUES ($1, $2, NOW());`;
-
-  const client = new Client(config);
-  try {
-    await client.connect();
-    await client.query(query, [amount, memo]);
-  } catch (e) {
-    logAndExit(e);
-  } finally {
-    client.end();
-  }
-}
-
-function printHelp() {
-  console.log(`
-An expense recording system
-
-Commands:
-
-add AMOUNT MEMO [DATE] - record a new expense
-clear - delete all expenses
-list - list all expenses
-delete NUMBER - remove expense with id NUMBER
-search QUERY - list expenses with a matching memo field`);
-}
-
-// MAIN
-if (argv[2] === "list") printAllExpenses(getAllExpenses());
-else if (argv[2] === "add") addExpense(argv[3], argv[4]);
-else if (argv[2] === "clear") console.log("deleting all expenses");
-else printHelp();
+const cli = new CLI();
+cli.main(argv);
